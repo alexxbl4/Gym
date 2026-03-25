5fd8730f0bd4e17d6db82319ea5987dcc60b3f5e
alexxbl4 <104828176+alexxbl4@users.noreply.github.com>
2026-03-25 09:24:17 +0100

    Create constants.js

---
+++ js/constants.js
@@ -0,0 +1,32 @@
+export const STORAGE_KEYS = {
+  routines: 'moonpro_routines',
+};
+
+export const DEFAULT_REST = 90;
+
+export const DEFAULT_EXERCISES = [
+  { name: 'Press Banca Plano', cat: 'Pecho' },
+  { name: 'Dominadas', cat: 'Espalda' },
+  { name: 'Press Militar Barra', cat: 'Hombro' },
+  { name: 'Curl Barra', cat: 'Bíceps' },
+  { name: 'Press Francés', cat: 'Tríceps' },
+  { name: 'Sentadilla Barra', cat: 'Pierna' },
+  { name: 'Plancha', cat: 'Core' },
+];
+
+export const CARDIO_KEYWORDS = [
+  'cinta',
+  'bici',
+  'bicicleta',
+  'cardio',
+  'elíptica',
+  'eliptica',
+  'escaladora',
+  'saltar',
+  'comba',
+  'caminar',
+  'correr',
+  'natación',
+  'nadar',
+  'remo',
+];

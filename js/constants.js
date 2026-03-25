6f8e5705177584d3cdeb29370b238b5d9e503d05
alexxbl4 <104828176+alexxbl4@users.noreply.github.com>
2026-03-25 11:50:23 +0100

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,63 +1,28 @@
+export const APP_SCHEMA_VERSION = 1;
+
 export const STORAGE_KEYS = {
-  app: 'moonpro_app_v4',
+  app: 'moonpro_app_data_v1',
 };
 
-export const APP_SCHEMA_VERSION = 4;
+export const CARDIO_KEYWORDS = ['correr', 'cinta', 'bici', 'elíptica', 'remo', 'andar'];
 export const DEFAULT_REST = 90;
 
-export const LIBRARY_CATEGORIES = [
-  'Todos',
-  'Pecho',
-  'Espalda',
-  'Hombro',
-  'Bíceps',
-  'Tríceps',
-  'Pierna',
-  'Core',
-  'Cardio',
-  'Mis ejercicios',
-];
+export const LIBRARY_CATEGORIES = ['Todos', 'Mis ejercicios', 'Pecho', 'Espalda', 'Pierna', 'Hombro', 'Brazo', 'Cardio'];
 
 export const DEFAULT_LIBRARY = [
   { name: 'Press Banca Plano', cat: 'Pecho' },
-  { name: 'Press Banca Inclinado', cat: 'Pecho' },
-  { name: 'Aperturas Mancuernas', cat: 'Pecho' },
+  { name: 'Press Inclinado Mancuernas', cat: 'Pecho' },
+  { name: 'Aperturas en Polea', cat: 'Pecho' },
   { name: 'Dominadas', cat: 'Espalda' },
-  { name: 'Remo Barra', cat: 'Espalda' },
+  { name: 'Remo con Barra', cat: 'Espalda' },
   { name: 'Jalón al Pecho', cat: 'Espalda' },
-  { name: 'Press Militar Barra', cat: 'Hombro' },
+  { name: 'Sentadilla Libre', cat: 'Pierna' },
+  { name: 'Prensa', cat: 'Pierna' },
+  { name: 'Peso Muerto Rumano', cat: 'Pierna' },
+  { name: 'Curl Femoral Tumbado', cat: 'Pierna' },
+  { name: 'Press Militar', cat: 'Hombro' },
   { name: 'Elevaciones Laterales', cat: 'Hombro' },
-  { name: 'Face Pull', cat: 'Hombro' },
-  { name: 'Curl Barra', cat: 'Bíceps' },
-  { name: 'Curl Martillo', cat: 'Bíceps' },
-  { name: 'Curl Scott', cat: 'Bíceps' },
-  { name: 'Press Francés', cat: 'Tríceps' },
-  { name: 'Extensión Polea Alta', cat: 'Tríceps' },
-  { name: 'Fondos Tríceps', cat: 'Tríceps' },
-  { name: 'Sentadilla Barra', cat: 'Pierna' },
-  { name: 'Prensa de Piernas', cat: 'Pierna' },
-  { name: 'Hip Thrust', cat: 'Pierna' },
-  { name: 'Plancha', cat: 'Core' },
-  { name: 'Crunch', cat: 'Core' },
-  { name: 'Rueda Abdominal', cat: 'Core' },
-  { name: 'Cinta', cat: 'Cardio' },
-  { name: 'Bicicleta', cat: 'Cardio' },
-  { name: 'Remo', cat: 'Cardio' },
-];
-
-export const CARDIO_KEYWORDS = [
-  'cinta',
-  'bici',
-  'bicicleta',
-  'cardio',
-  'elíptica',
-  'eliptica',
-  'escaladora',
-  'saltar',
-  'comba',
-  'caminar',
-  'correr',
-  'natación',
-  'nadar',
-  'remo',
+  { name: 'Curl Bíceps Barra', cat: 'Brazo' },
+  { name: 'Extensión Tríceps Polea', cat: 'Brazo' },
+  { name: 'Cinta de correr', cat: 'Cardio' }
 ];

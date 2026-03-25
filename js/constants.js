9b729bab536487f70465a4957ea445c44669f260
alexxbl4 <104828176+alexxbl4@users.noreply.github.com>
2026-03-25 09:51:06 +0100

    Actualizar constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,9 +1,49 @@
 export const STORAGE_KEYS = {
-  app: 'moonpro_app_v3',
+  app: 'moonpro_app_v4',
 };
 
-export const APP_SCHEMA_VERSION = 3;
+export const APP_SCHEMA_VERSION = 4;
 export const DEFAULT_REST = 90;
+
+export const LIBRARY_CATEGORIES = [
+  'Todos',
+  'Pecho',
+  'Espalda',
+  'Hombro',
+  'Bíceps',
+  'Tríceps',
+  'Pierna',
+  'Core',
+  'Cardio',
+  'Mis ejercicios',
+];
+
+export const DEFAULT_LIBRARY = [
+  { name: 'Press Banca Plano', cat: 'Pecho' },
+  { name: 'Press Banca Inclinado', cat: 'Pecho' },
+  { name: 'Aperturas Mancuernas', cat: 'Pecho' },
+  { name: 'Dominadas', cat: 'Espalda' },
+  { name: 'Remo Barra', cat: 'Espalda' },
+  { name: 'Jalón al Pecho', cat: 'Espalda' },
+  { name: 'Press Militar Barra', cat: 'Hombro' },
+  { name: 'Elevaciones Laterales', cat: 'Hombro' },
+  { name: 'Face Pull', cat: 'Hombro' },
+  { name: 'Curl Barra', cat: 'Bíceps' },
+  { name: 'Curl Martillo', cat: 'Bíceps' },
+  { name: 'Curl Scott', cat: 'Bíceps' },
+  { name: 'Press Francés', cat: 'Tríceps' },
+  { name: 'Extensión Polea Alta', cat: 'Tríceps' },
+  { name: 'Fondos Tríceps', cat: 'Tríceps' },
+  { name: 'Sentadilla Barra', cat: 'Pierna' },
+  { name: 'Prensa de Piernas', cat: 'Pierna' },
+  { name: 'Hip Thrust', cat: 'Pierna' },
+  { name: 'Plancha', cat: 'Core' },
+  { name: 'Crunch', cat: 'Core' },
+  { name: 'Rueda Abdominal', cat: 'Core' },
+  { name: 'Cinta', cat: 'Cardio' },
+  { name: 'Bicicleta', cat: 'Cardio' },
+  { name: 'Remo', cat: 'Cardio' },
+];
 
 export const CARDIO_KEYWORDS = [
   'cinta',

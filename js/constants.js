d55e16ffa1e2770afc81d921f36ea8ff654cbef7
alexxbl4 <104828176+alexxbl4@users.noreply.github.com>
2026-03-26 09:05:45 +0100

    Actualizar constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,92 +1,91 @@
-export const APP_SCHEMA_VERSION = 1;
+export const APP_SCHEMA_VERSION = 2; // Subimos a v2 por el cambio de trackType
 
 export const STORAGE_KEYS = {
-  app: 'moonpro_app_data_v1',
+  app: 'moonpro_app_data_v2',
 };
 
-export const CARDIO_KEYWORDS = ['correr', 'cinta', 'bici', 'elíptica', 'remo', 'andar', 'saltar', 'comba'];
 export const DEFAULT_REST = 90;
 
-export const LIBRARY_CATEGORIES = ['Todos', 'Mis ejercicios', 'Pecho', 'Espalda', 'Pierna', 'Hombro', 'Brazo', 'Core', 'Cardio'];
+export const LIBRARY_CATEGORIES = [
+  'Todos', 'Mis ejercicios', 'Pecho', 'Espalda', 'Pierna', 
+  'Hombro', 'Brazo', 'Core', 'Calistenia', 'Cardio', 'Boxeo'
+];
 
 export const DEFAULT_LIBRARY = [
   // Pecho
-  { name: 'Press Banca Plano', cat: 'Pecho' },
-  { name: 'Press Banca Inclinado', cat: 'Pecho' },
-  { name: 'Press Banca Declinado', cat: 'Pecho' },
-  { name: 'Press Inclinado Mancuernas', cat: 'Pecho' },
-  { name: 'Press Plano Mancuernas', cat: 'Pecho' },
-  { name: 'Aperturas con Mancuernas', cat: 'Pecho' },
-  { name: 'Aperturas en Polea', cat: 'Pecho' },
-  { name: 'Cruces en Polea', cat: 'Pecho' },
-  { name: 'Peck Deck (Máquina)', cat: 'Pecho' },
-  { name: 'Fondos en Paralelas (Pecho)', cat: 'Pecho' },
-  { name: 'Flexiones (Push-ups)', cat: 'Pecho' },
+  { name: 'Press Banca Plano', cat: 'Pecho', trackType: 'weight_reps' },
+  { name: 'Press Banca Inclinado', cat: 'Pecho', trackType: 'weight_reps' },
+  { name: 'Aperturas con Mancuernas', cat: 'Pecho', trackType: 'weight_reps' },
+  { name: 'Cruces en Polea', cat: 'Pecho', trackType: 'weight_reps' },
+  { name: 'Peck Deck (Máquina)', cat: 'Pecho', trackType: 'weight_reps' },
 
   // Espalda
-  { name: 'Dominadas', cat: 'Espalda' },
-  { name: 'Dominadas Supinas', cat: 'Espalda' },
-  { name: 'Jalón al Pecho', cat: 'Espalda' },
-  { name: 'Jalón al Pecho Agarre Estrecho', cat: 'Espalda' },
-  { name: 'Remo con Barra', cat: 'Espalda' },
-  { name: 'Remo Pendlay', cat: 'Espalda' },
-  { name: 'Remo con Mancuerna', cat: 'Espalda' },
-  { name: 'Remo en Polea Baja', cat: 'Espalda' },
-  { name: 'Remo en Máquina', cat: 'Espalda' },
-  { name: 'Pullover en Polea Alta', cat: 'Espalda' },
-  { name: 'Peso Muerto Convencional', cat: 'Espalda' },
+  { name: 'Jalón al Pecho', cat: 'Espalda', trackType: 'weight_reps' },
+  { name: 'Remo con Barra', cat: 'Espalda', trackType: 'weight_reps' },
+  { name: 'Remo con Mancuerna', cat: 'Espalda', trackType: 'weight_reps' },
+  { name: 'Remo Gironda', cat: 'Espalda', trackType: 'weight_reps' },
+  { name: 'Pullover en Polea', cat: 'Espalda', trackType: 'weight_reps' },
+  { name: 'Peso Muerto Convencional', cat: 'Espalda', trackType: 'weight_reps' },
 
   // Pierna
-  { name: 'Sentadilla Libre', cat: 'Pierna' },
-  { name: 'Sentadilla Hack', cat: 'Pierna' },
-  { name: 'Sentadilla Búlgara', cat: 'Pierna' },
-  { name: 'Sentadilla Frontal', cat: 'Pierna' },
-  { name: 'Prensa', cat: 'Pierna' },
-  { name: 'Zancadas (Lunges)', cat: 'Pierna' },
-  { name: 'Extensión de Cuádriceps', cat: 'Pierna' },
-  { name: 'Peso Muerto Rumano', cat: 'Pierna' },
-  { name: 'Curl Femoral Tumbado', cat: 'Pierna' },
-  { name: 'Curl Femoral Sentado', cat: 'Pierna' },
-  { name: 'Hip Thrust', cat: 'Pierna' },
-  { name: 'Elevación de Talones (Gemelos)', cat: 'Pierna' },
-  { name: 'Gemelos en Prensa', cat: 'Pierna' },
+  { name: 'Sentadilla Libre', cat: 'Pierna', trackType: 'weight_reps' },
+  { name: 'Prensa', cat: 'Pierna', trackType: 'weight_reps' },
+  { name: 'Sentadilla Búlgara', cat: 'Pierna', trackType: 'weight_reps' },
+  { name: 'Extensión de Cuádriceps', cat: 'Pierna', trackType: 'weight_reps' },
+  { name: 'Peso Muerto Rumano', cat: 'Pierna', trackType: 'weight_reps' },
+  { name: 'Curl Femoral Tumbado', cat: 'Pierna', trackType: 'weight_reps' },
+  { name: 'Hip Thrust', cat: 'Pierna', trackType: 'weight_reps' },
+  { name: 'Elevación de Gemelos', cat: 'Pierna', trackType: 'weight_reps' },
 
   // Hombro
-  { name: 'Press Militar con Barra', cat: 'Hombro' },
-  { name: 'Press Militar Mancuernas', cat: 'Hombro' },
-  { name: 'Press Arnold', cat: 'Hombro' },
-  { name: 'Elevaciones Laterales', cat: 'Hombro' },
-  { name: 'Elevaciones Laterales en Polea', cat: 'Hombro' },
-  { name: 'Elevaciones Frontales', cat: 'Hombro' },
-  { name: 'Pájaros (Posterior con Mancuernas)', cat: 'Hombro' },
-  { name: 'Face Pull', cat: 'Hombro' },
-  { name: 'Encogimientos (Trapecio)', cat: 'Hombro' },
+  { name: 'Press Militar', cat: 'Hombro', trackType: 'weight_reps' },
+  { name: 'Elevaciones Laterales', cat: 'Hombro', trackType: 'weight_reps' },
+  { name: 'Elevaciones Frontales', cat: 'Hombro', trackType: 'weight_reps' },
+  { name: 'Pájaros (Posterior)', cat: 'Hombro', trackType: 'weight_reps' },
+  { name: 'Face Pull', cat: 'Hombro', trackType: 'weight_reps' },
 
   // Brazo
-  { name: 'Curl Bíceps Barra', cat: 'Brazo' },
-  { name: 'Curl Bíceps Mancuernas', cat: 'Brazo' },
-  { name: 'Curl Martillo', cat: 'Brazo' },
-  { name: 'Curl en Banco Scott', cat: 'Brazo' },
-  { name: 'Curl en Polea Baja', cat: 'Brazo' },
-  { name: 'Extensión Tríceps Polea (Cuerda)', cat: 'Brazo' },
-  { name: 'Extensión Tríceps Polea (Barra)', cat: 'Brazo' },
-  { name: 'Press Francés', cat: 'Brazo' },
-  { name: 'Patada de Tríceps', cat: 'Brazo' },
-  { name: 'Fondos de Tríceps', cat: 'Brazo' },
+  { name: 'Curl Bíceps Barra', cat: 'Brazo', trackType: 'weight_reps' },
+  { name: 'Curl Bíceps Mancuernas', cat: 'Brazo', trackType: 'weight_reps' },
+  { name: 'Extensión Tríceps Polea', cat: 'Brazo', trackType: 'weight_reps' },
+  { name: 'Press Francés', cat: 'Brazo', trackType: 'weight_reps' },
 
-  // Core (Abdominales)
-  { name: 'Crunch Abdominal', cat: 'Core' },
-  { name: 'Plancha (Plank)', cat: 'Core' },
-  { name: 'Elevación de Piernas Colgado', cat: 'Core' },
-  { name: 'Rueda Abdominal (Ab Wheel)', cat: 'Core' },
-  { name: 'Russian Twists', cat: 'Core' },
-  { name: 'Crunch en Polea', cat: 'Core' },
+  // Core
+  { name: 'Crunch Abdominal', cat: 'Core', trackType: 'reps_only' },
+  { name: 'Crunch en Polea', cat: 'Core', trackType: 'weight_reps' },
+  { name: 'Plancha (Plank)', cat: 'Core', trackType: 'time_only' },
+  { name: 'Elevación de Piernas', cat: 'Core', trackType: 'reps_only' },
+  { name: 'Rueda Abdominal', cat: 'Core', trackType: 'reps_only' },
 
-  // Cardio
-  { name: 'Cinta de correr', cat: 'Cardio' },
-  { name: 'Bicicleta Estática', cat: 'Cardio' },
-  { name: 'Bicicleta Elíptica', cat: 'Cardio' },
-  { name: 'Máquina de Remo', cat: 'Cardio' },
-  { name: 'Salto a la Comba', cat: 'Cardio' },
-  { name: 'Stairmaster (Escaleras)', cat: 'Cardio' }
+  // Calistenia / Peso Corporal (Reps only)
+  { name: 'Dominadas', cat: 'Calistenia', trackType: 'reps_only' },
+  { name: 'Dominadas Supinas', cat: 'Calistenia', trackType: 'reps_only' },
+  { name: 'Flexiones (Push-ups)', cat: 'Calistenia', trackType: 'reps_only' },
+  { name: 'Fondos en Paralelas', cat: 'Calistenia', trackType: 'reps_only' },
+  { name: 'Burpees', cat: 'Calistenia', trackType: 'reps_only' },
+  { name: 'Muscle-ups', cat: 'Calistenia', trackType: 'reps_only' },
+  
+  // Lastrados
+  { name: 'Dominadas Lastradas', cat: 'Espalda', trackType: 'weight_reps' },
+  { name: 'Fondos Lastrados', cat: 'Pecho', trackType: 'weight_reps' },
+
+  // Cardio (Time only)
+  { name: 'Cinta de correr', cat: 'Cardio', trackType: 'time_only' },
+  { name: 'Bicicleta Estática', cat: 'Cardio', trackType: 'time_only' },
+  { name: 'Elíptica', cat: 'Cardio', trackType: 'time_only' },
+  { name: 'Salto a la Comba', cat: 'Cardio', trackType: 'time_only' },
+
+  // Boxeo (Time only)
+  { name: 'Boxeo Saco', cat: 'Boxeo', trackType: 'time_only' },
+  { name: 'Sombra de Boxeo', cat: 'Boxeo', trackType: 'time_only' },
+  { name: 'Manoplas', cat: 'Boxeo', trackType: 'time_only' },
+  { name: 'Sparring', cat: 'Boxeo', trackType: 'time_only' }
 ];
+
+export function getTrackTypeOptions() {
+  return [
+    { value: 'weight_reps', label: 'Peso + Reps' },
+    { value: 'reps_only', label: 'Solo Reps' },
+    { value: 'time_only', label: 'Tiempo (Min:Seg)' }
+  ];
+}
